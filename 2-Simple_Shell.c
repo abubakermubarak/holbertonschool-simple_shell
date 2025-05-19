4f1a67ccbe967b8b904ac2a0ae2f5f4d5b59bcad
Monica-Holberton <monicafarid7@gmail.com>
2025-05-19 12:15:26 +0000

    Simple Shell!

---
+++ 2-Simple_Shell.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - A very simple UNIX command line interpreter
+ *
+ * Description:
+ * - Displays a prompt ($)
+ * - Reads a command (one word only)
+ * - Executes it using execve
+ * - Print an error if the command doesn't exist
+ * - Repeats until Ctrl+D (EOF)
+ *
+ * Return: Always 0
+ */
+
+ void __attribute__ ((constructor)) premain()
+ {
+	 printf("$ ");
+ }
+ int main(void)
+ {
+	 char *lptr = NULL;
+	 size_t len = 0;
+	 ssize_t read;
+ 
+	 /* Read lines until EOF (Ctrl+D) */
+	 while ((read = getline(&lptr, &len, stdin)) != -1)
+	 {
+		 printf("%s", lptr);
+		 printf("$ ");
+	 }
+ 
+	 free(lptr);
+	 printf("\n"); /* Clean newline after Ctrl+D */
+	 return (0);
+ }
