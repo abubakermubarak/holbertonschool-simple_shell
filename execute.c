48e8162385b021a2b1d6384f5e99758c099733c2
abubakermubarak <abubakermubarak95@gmail.com>
2025-05-22 10:11:22 +0000

    modify execute function

--- execute.c
+++ execute.c
@@ -1,31 +1,11 @@
 #include "main.h"
-#include <string.h>
-int execute(char *line, size_t *len, char *argv[])
+#include <unistd.h>
+
+int execute(char *argv[])
 {
-    ssize_t read;
-    pid_t pid;
-    while ((read = getline(&lptr, &len, stdin)) != -1)
+    if (execve(argv[0], argv, NULL) == -1)
     {
-        if (read > 0 && lptr[read - 1] == '\n')
-        {
-            lptr[read - 1] = '\0';
-        }
-        argv[0] = lptr;
-        argv[3] = NULL;
-        pid = fork();
-        if (pid == -1)
-        {
-            exit(1)
-        }
-        if (pid == 0)
-        {
-            if (lptr[0] != '\n')
-            {
-                if (execve(argv[0], argv, NULL) == -1)
-                {
-                    perror("ERRor");
-                }
-            }        
-        }
+        perror("Error:");
     }
+    return (0);
 }
