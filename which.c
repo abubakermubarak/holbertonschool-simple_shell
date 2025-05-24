d4ffe27f03d2255ccda42053192bba91fa1b77e2
abubakermubarak <abubakermubarak95@gmail.com>
2025-05-24 09:06:48 +0000

    A function that checks if a command is in a path

--- which.c
+++ which.c
@@ -1,17 +1,48 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-char* _which(char* file)
+
+#include "holberton.h"
+
+
+char * _which(char * filename)
 {
-    char *path = getenv("PATH");
-    printf("%s\n", path);
-    char *dir = malloc(sizeof( char *));
-    int i = 0;
-    dir = strtok(path, ":");
-    printf("%s", dir[0]);
+int i;
+int len;
+struct stat st;
+char *path = getenv("PATH");
+    //filename=ls
+char **dir = split(path, ":");
+len = sizeof(dir);
+char* paths[len];
+
+for (i = 0; i<len-1; i++)
+{
+    int l = strlen(dir[i]);  
+  
+    // Add the new character at the end
+    dir[i][l] = '/';
+  
+    // Add the null terminator
+    dir[i][l + 1] = '\0';
+    paths[i]=strcat(dir[i],filename);
+    
+}
+
+i=0;
+while (paths[i])
+    {
+        printf("%s:", paths[i]);
+        if (stat(paths[i], &st) == 0)
+        {
+            printf(" FOUND\n");
+            return paths[i];
+        }
+        else
+        {
+            printf("Not correct \n");
+            
+        }
+        i++;
+    }
+    return NULL;
 }
 int main(void)
 {
