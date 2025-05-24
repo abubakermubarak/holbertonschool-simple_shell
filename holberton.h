07e54155c6b51e8d068ea038257ad72bedb94484
abubakermubarak <abubakermubarak95@gmail.com>
2025-05-24 09:07:19 +0000

    add split and _which prototypes

--- holberton.h
+++ holberton.h
@@ -1,4 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
 int execute(char *argv[]);
+char** split(char *string, char *delimiter);
 #endif
