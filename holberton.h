af6d68c9a786f53c207e5610bb4314d6ede5231c
peterHassaballah <peter.hassaballah@gmail.com>
2025-05-26 12:42:01 +0000

    chore: modify .h for the PR

--- holberton.h
+++ holberton.h
@@ -16,9 +16,32 @@ typedef struct node
     char *str;
     struct node* next;
 }node;
-int execute(char *argv[]);
+
 char** split(char *string, char *delimiter);
 node* append(node *head , char *string);
 node* get_path(char **path);
 bool is_built_in(char *command);
+
+int is_input(char *command);  /* Returns True or False */
+
+/* Prints the shell prompt ($) */
+void print_prompt(void);
+
+/* Reads command from standard input */
+ssize_t read_command(char **lineptr, size_t *n);
+
+/* Creates a child process to run a command */
+int execute(char *argv[]);
+
+/* Tokeniz the input line into arguments */
+void tokenize_command(char *line, char *argv);
+
+/* Fork and Executes a built-in command */
+int execute_builtin(char *command);
+
+/*Checks if the string is found in the Global Variable */
+bool in_path(char *string);
+
+/*Find a path */
+char *_which(const char *filename);
 #endif
