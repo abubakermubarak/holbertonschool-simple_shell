5adc0df2ffe545dc06e2aca512654e39e3027815
abubakermubarak <abubakermubarak95@gmail.com>
2025-05-28 15:11:51 +0000

    testing split function

---
+++ tests/test_split.c
@@ -0,0 +1,16 @@
+
+
+int main(void)
+{
+    
+    //char s[] = "This is a test"; 
+    //char s[] = "test";
+    //char s[] = " ";
+    // char s[] = NULL;
+    char *delm = " ";
+    char ** test  = split(s, delm); 
+    for (int i  = 0; test[i] != NULL; i++)
+    {
+        printf("%s\n", test[i]);
+    }
+}
