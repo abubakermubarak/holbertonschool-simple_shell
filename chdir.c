023d2873e8f8c485af0df7c8eb40a619ee6130c4
abubakermubarak <abubakermubarak95@gmail.com>
2025-06-01 13:34:57 +0000

    change the value of PWD

--- chdir.c
+++ chdir.c
@@ -1,4 +1,15 @@
+#include "holberton.h"
 int _chdir(char *path)
 {
+  //change current wotking dirctory
+  //set enviroment variable ""
+  setenv("PWD", path, 1);
 
+}
+int main(void)
+{
+    printf("current working dir: %s\n", getenv("PWD"));
+    _chdir("/root/");
+    printf("after cd\n");
+    printf("current working dir: %s\n", getenv("PWD"));
 }
