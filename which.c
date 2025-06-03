7b67c926a70e5b7cb856fd07373e495765cf454c
abubakermubarak <abubakermubarak95@gmail.com>
2025-06-03 12:30:41 +0000

    remove printf

--- which.c
+++ which.c
@@ -4,7 +4,7 @@
 
 char * _which(char * filename)
 {
-    printf("_which %s\n",filename);
+    //printf("_which %s\n",filename);
  st t;
 int len, i;
 char *path = getenv("PATH");
@@ -12,8 +12,8 @@ char *path = getenv("PATH");
 char **dir = split(path, ":");
 for (len = 0; dir[len]; len++);
 char **paths = malloc(sizeof(char *) * len);
-printf("%s\n", path);
-printf("len : %i\n", len);
+//printf("%s\n", path);
+//printf("len : %i\n", len);
 for (i = 0; i<len; i++)
 {
     int l = strlen(dir[i]);  
@@ -24,16 +24,16 @@ for (i = 0; i<len; i++)
     // Add the null terminator
     dir[i][l + 1] = '\0';
     paths[i]=strcat(dir[i],filename);
-    printf(" L26: %s\n", paths[i]);
+    //printf(" L26: %s\n", paths[i]);
 }
 
 i=0;
 while (paths[i])
     {
-        printf("L32: %s:", paths[i]);
+        //printf("L32: %s:", paths[i]);
         if (stat(paths[i], &t) == 0)
         {
-            printf(" FOUND\n");
+            //printf(" FOUND\n");
             return paths[i];
         }
         i++;
