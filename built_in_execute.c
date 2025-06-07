c45e285477536f1eba4351c6446afbd2d84d14a8
abubakermubarak <abubakermubarak95@gmail.com>
2025-06-07 15:09:06 +0000

    add another arguments to function

--- built_in_execute.c
+++ built_in_execute.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
-int execute_builtin(char *command)
+int execute_builtin(char *command, char *arg)
 {
+    int status;
+    status  = _atoi(arg);
     /* check if the command is exit*/
     if (strcmp(command, "exit") == 0)
     {
-        exit();
+        exit(status);
     }
     /* check if the command is env*/
     else if (strcmp(command, "env") == 0)
