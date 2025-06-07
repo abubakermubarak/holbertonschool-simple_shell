6ab2a6ca871e8fda20caf41f9574178663c2a4de
abubakermubarak <abubakermubarak95@gmail.com>
2025-06-07 15:18:33 +0000

    change comment style

--- which.c
+++ which.c
@@ -4,36 +4,36 @@
 
 char * _which(char * filename)
 {
-    //printf("_which %s\n",filename);
+    /*printf("_which %s\n",filename);*/
  st t;
 int len, i;
 char *path = getenv("PATH");
-    //filename=ls
+    /*filename=ls*/
 char **dir = split(path, ":");
 for (len = 0; dir[len]; len++);
 char **paths = malloc(sizeof(char *) * len);
-//printf("%s\n", path);
-//printf("len : %i\n", len);
+/*printf("%s\n", path);*/
+/*printf("len : %i\n", len);*/
 for (i = 0; i<len; i++)
 {
     int l = strlen(dir[i]);  
   
-    // Add the new character at the end
+    /* Add the new character at the end */
     dir[i][l] = '/';
   
-    // Add the null terminator
+    /* Add the null terminator */
     dir[i][l + 1] = '\0';
     paths[i]=strcat(dir[i],filename);
-    //printf(" L26: %s\n", paths[i]);
+    /*printf(" L26: %s\n", paths[i]);*/
 }
 
 i=0;
 while (paths[i])
     {
-        //printf("L32: %s:", paths[i]);
+        /*printf("L32: %s:", paths[i]);*/
         if (stat(paths[i], &t) == 0)
         {
-            //printf(" FOUND\n");
+            /*printf(" FOUND\n");*/
             return paths[i];
         }
         i++;
@@ -42,6 +42,6 @@ while (paths[i])
     free(paths[j]);
 }
 free(paths);
-    printf("NOT FOUND\n");
+    /*printf("NOT FOUND\n");*/
     return NULL;
 }
